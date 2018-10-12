f6125193b82544cc5f38cb9e90517fc524a01761
Rahul Nath <43341760+rahulnath5@users.noreply.github.com>
2018-10-13 02:44:43 +0530

    borrow class added

---
+++ LibraryManagementSystemCA/src/Database/Borrow.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Rahul
+ */
+public class Borrow {
+    
+    public int borrow_id;
+    public int person_id;
+    public int book_id;
+    public String borrowed_on;
+     
+    public Borrow(int person_id, int book_id, String borrowed_on){
+        this.person_id=person_id;
+        this.book_id=book_id;
+        this.borrowed_on=borrowed_on;
+    }
+    
+    public void insertBorrow(Connection con) throws SQLException {
+        String sql = "insert into borrow values(null, ?, ?, null);";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, person_id);
+        ps.setInt(2, book_id);
+        ps.executeUpdate();
+        ps.close();
+               
+    }
+    
+    public void getBorrow(Connection con) throws SQLException  {
+        String sql = "select * from borrow;";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()){
+            System.out.println(rs.getInt(1));
+            System.out.println(rs.getInt(2));
+            System.out.println(rs.getString(3));
+        }
+        rs.close();
+        ps.close();
+    }
+}
