d23ffcfac79e8b0ce76416611df3a4a6755ffa74
calvertcofficial99 <43202284+calvertcofficial99@users.noreply.github.com>
2018-10-12 16:36:38 +0530

    added Database Connection file to the package

---
+++ LibraryManagementSystemCA/src/Database/Database.java
@@ -0,0 +1,24 @@
+
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    Connection con;
+    String password = "root";
+    String uri = "jdbc:mysql://localhost:3306/LibraryManagementSystem";
+    String user = "root";
+    
+    
+    public Connection openConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        con = DriverManager.getConnection(uri, user, password);
+        return con;
+    }
+    
+    public void closeConnection() throws SQLException{
+    	con.close();
+    }
+}
